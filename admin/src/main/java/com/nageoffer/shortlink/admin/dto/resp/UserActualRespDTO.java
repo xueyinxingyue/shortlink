d9e3640b13ef80c9d3d4417e8b84a11181187a11
雪银星月 <425899311@qq.com>
2025-08-18 00:56:36 +0800

    feature:开发短链接后管系统查询用户信息脱敏功能

--- admin/src/main/java/com/nageoffer/shortlink/admin/dto/resp/UserActualRespDTO.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/dto/resp/UserActualRespDTO.java
@@ -1,14 +1,12 @@
 package com.nageoffer.shortlink.admin.dto.resp;
 
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.nageoffer.shortlink.admin.common.serialize.PhoneDesensitizationSerializer;
 import lombok.Data;
 
 /**
- * 用户返回参数响应
+ * 用户返回参数响应（无脱敏）
  */
 @Data
-public class UserRespDTO {
+public class UserActualRespDTO {
 
     /**
      * ID
@@ -28,7 +26,6 @@ public class UserRespDTO {
     /**
      * 手机号
      */
-    @JsonSerialize(using = PhoneDesensitizationSerializer.class)
     private String phone;
 
     /**
