4153825dee4f909bea93182a37afe404fa244265
雪银星月 <425899311@qq.com>
2025-08-19 02:47:34 +0800

    feature：通过分布式锁完善用户注册功能，防止恶意请求短时间使用统一未注册用户名注册新用户

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/common/constant/RedisCacheConstant.java
@@ -0,0 +1,9 @@
+package com.nageoffer.shortlink.admin.common.constant;
+
+/**
+ * 短链接后管 Redis 缓存常量类
+ */
+public class RedisCacheConstant {
+
+    public static final String LOCK_USER_register_KEY = "short-link:lock_user_register:";
+}
