7487034e8b556ec1a7abc17e4c4c91cf43782774
雪银星月 <425899311@qq.com>
2025-08-23 17:15:16 +0800

    feature：开发短链接后管系统用户登陆功能

--- admin/src/main/java/com/nageoffer/shortlink/admin/common/constant/RedisCacheConstant.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/common/constant/RedisCacheConstant.java
@@ -5,5 +5,13 @@ package com.nageoffer.shortlink.admin.common.constant;
  */
 public class RedisCacheConstant {
 
+    /**
+     * 用户注册分布式锁
+     */
     public static final String LOCK_USER_register_KEY = "short-link:lock_user_register:";
+
+    /**
+     * 用户登录缓存标识
+     */
+    public static final String USER_LOGIN_KEY = "short-link:login:";
 }
