4006e01a0e6f492b2a89d0157a8c823895e4c6f9
雪银星月 <425899311@qq.com>
2025-08-23 23:52:23 +0800

    optimize：统一注释风格

--- admin/src/main/java/com/nageoffer/shortlink/admin/service/UserService.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/service/UserService.java
@@ -15,6 +15,7 @@ public interface UserService extends IService<UserDO> {
 
     /**
      * 根据用户名查询用户信息
+     *
      * @param username 用户名
      * @return 用户返回实体
      */
@@ -22,6 +23,7 @@ public interface UserService extends IService<UserDO> {
 
     /**
      * 查询用户名是否存在
+     *
      * @param username 用户名
      * @return 用户名存在返回 true, 不存在返回 false
      */
@@ -29,18 +31,21 @@ public interface UserService extends IService<UserDO> {
 
     /**
      * 注册用户
+     *
      * @param requestParam 注册用户请求参数
      */
     void register(UserRegisterReqDTO requestParam);
 
     /**
      * 根据用户名修改用户
+     *
      * @param requestParam 修改用户请求参数
      */
     void update(UserUpdateReqDTO requestParam);
 
     /**
      * 用户登录
+     *
      * @param requestParam 用户登录请求参数
      * @return 用户登录返回参数 Token
      */
@@ -48,6 +53,7 @@ public interface UserService extends IService<UserDO> {
 
     /**
      * 检查用户是否登录
+     *
      * @param username 用户名
      * @param token    用户登录 Token
      * @return 用户是否登录标识
@@ -56,6 +62,7 @@ public interface UserService extends IService<UserDO> {
 
     /**
      * 退出登录
+     *
      * @param username 用户名
      * @param token    用户登录 Token
      */
