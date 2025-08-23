d3520f1f00b7bba202587536c2bc2927422fcec7
雪银星月 <425899311@qq.com>
2025-08-23 23:53:27 +0800

    optimize：优化用户持久层实体基础属性抽象

--- admin/src/main/java/com/nageoffer/shortlink/admin/dao/entity/GroupDO.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/dao/entity/GroupDO.java
@@ -1,14 +1,21 @@
 package com.nageoffer.shortlink.admin.dao.entity;
 
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.nageoffer.shortlink.admin.common.database.BaseDO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * 短链接分组实体
  */
 @Data
 @TableName("t_group")
-public class GroupDO {
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class GroupDO extends BaseDO {
 
     /**
      * id
