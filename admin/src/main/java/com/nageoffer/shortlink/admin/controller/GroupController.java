7a1e423e10b0e6832519ff4c3310fa97cde445a0
雪银星月 <425899311@qq.com>
2025-08-23 23:54:21 +0800

    feature：开发短链接后管分组新增功能

--- admin/src/main/java/com/nageoffer/shortlink/admin/controller/GroupController.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/controller/GroupController.java
@@ -1,7 +1,12 @@
 package com.nageoffer.shortlink.admin.controller;
 
+import com.nageoffer.shortlink.admin.common.convention.result.Result;
+import com.nageoffer.shortlink.admin.common.convention.result.Results;
+import com.nageoffer.shortlink.admin.dto.req.ShortLinkGroupSaveReqDTO;
 import com.nageoffer.shortlink.admin.service.GroupService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -12,4 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class GroupController {
 
     private final GroupService groupService;
+
+    /**
+     * 新增短链接分组
+     */
+    @PostMapping("/api/short-link/admin/v1/group")
+    public Result<Void> save(@RequestBody ShortLinkGroupSaveReqDTO requestParam) {
+        groupService.saveGroup(requestParam.getName());
+        return Results.success();
+    }
 }
