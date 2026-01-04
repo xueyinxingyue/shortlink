0a242575e957225a4ad57a643c0761901b3b1070
雪银星月 <425899311@qq.com>
2026-01-05 03:39:44 +0800

    feature: 新增短链接分组分库分表数据库语句

---
+++ admin/src/test/java/com/nageoffer/shortlink/admin/test/GroupTableShardingTest.java
@@ -0,0 +1,23 @@
+package com.nageoffer.shortlink.admin.test;
+
+public class GroupTableShardingTest {
+
+    public static final String SQL = "CREATE TABLE `t_group_%d` (\n" +
+            "  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'ID',\n" +
+            "  `gid` varchar(32) DEFAULT NULL COMMENT '分组标识',\n" +
+            "  `name` varchar(64) DEFAULT NULL COMMENT '分组名称',\n" +
+            "  `username` varchar(256) DEFAULT NULL COMMENT '创建分组用户名',\n" +
+            "  `sort_order` int DEFAULT NULL COMMENT '分组排序',\n" +
+            "  `create_time` datetime DEFAULT NULL COMMENT '创建时间',\n" +
+            "  `update_time` datetime DEFAULT NULL COMMENT '修改时间',\n" +
+            "  `del_flag` tinyint(1) DEFAULT NULL COMMENT '删除标识 0：未删除 1：已删除',\n" +
+            "  PRIMARY KEY (`id`),\n" +
+            "  UNIQUE KEY `idx_unique_username_gid` (`gid`,`username`) USING BTREE\n" +
+            ") ENGINE=InnoDB AUTO_INCREMENT=1993760089229238274 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;";
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 16; i++) {
+            System.out.printf((SQL) + "%n", i);
+        }
+    }
+}
